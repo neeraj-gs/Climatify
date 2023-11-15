9e405eaff4bbb0a847fe735b9e5922a01020e3b0
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 23:59:28 +0530

    Added Srtict types to LiveClock

--- climatify-typescript/src/components/LiveClock.tsx
+++ climatify-typescript/src/components/LiveClock.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 const LiveClock = () => {
-    let time = new Date().toLocaleTimeString();
+    const time = new Date().toLocaleTimeString();
     const [curTime,setCurTime] = useState(time);
 
     const updateTime = ()=>{
-        let time = new Date().toLocaleTimeString();
+        const time = new Date().toLocaleTimeString();
         setCurTime(time)
     }
 
